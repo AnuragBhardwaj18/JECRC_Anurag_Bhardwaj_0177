ffcabb62259f7963660cc417bd3103cb68d80b0e
AnuragBhardwaj18 <anuragbhardwaj1883@gmail.com>
2026-02-28 11:48:36 +0530

    new

---
+++ 27-02-2026/WebApplication2/WebApplication2/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product{ Id=1,Name="Laptop",Price=50000},
+            new Product{ Id=2,Name="Mobile",Price=25000},
+            new Product{ Id=3,Name="Speaker",Price=5000}
+        };
+
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+    }
+}
